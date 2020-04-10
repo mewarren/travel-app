778a46862c1580f51cfae46de4d80e605361ce72
mewarren <holla@mewarren.com>
2020-04-09 21:33:57 -0700

    updated app, add parallax

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -4,7 +4,13 @@ import Template from '../Template';
 
 const template = new Template(html);
 
-export default class Content{
+export default class Content {
+
+  // document.window.addEventListener('scroll', function(e) {
+  //   let scrolled = window.pageYOffset;
+  //   const background = document.querySelector('.color-transition');
+  //   background.getElementsByClassName.top = -(scrolled * 0.2) + 'px';
+  // }); 
   
   render(){
     return template.clone();
--- src/main.js
+++ src/main.js
@@ -5,3 +5,25 @@ import App from './components/app/App';
 const root = document.getElementById('root');
 const app = new App();
 root.appendChild(app.render());
+
+window.addEventListener('scroll', function(e) {
+
+// console.log('scroll!!');
+  let scrolled = window.pageYOffset;
+  const transition = document.querySelector('.color-transition');
+//   const wave = document.getElementById('wave2');
+    
+//   wave.style.top = -(scrolled * 1.25)  + 'px';
+  transition.style.top = -(scrolled * 1.5) + 'px';
+});
+
+window.addEventListener('scroll', function(e) {
+
+    // console.log('scroll!!');
+      let scrolled = window.pageYOffset;
+      const wave = document.querySelector('.wave2');
+    //   const wave = document.getElementById('wave2');
+        
+    //   wave.style.top = -(scrolled * 1.25)  + 'px';
+      wave.style.top = -(scrolled * 1.25) + 'px';
+    });
